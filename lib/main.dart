4b26119d0d2eb93de7f82ab4d291eb85ea28a420
Cesar Murillo <namiayumis@gmail.com>
2019-07-19 15:25:14 +0900

    refactor

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:curso_dart/src/app.dart';
+
+
+void main() => runApp(MyApp());
+
+
+
+
---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:curso_dart/src/screens/myHomePage.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
